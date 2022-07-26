4b4fffd1083d1ec75c1b05ce2d4b338b98efb65d
Jonathan Aaron <jon@finesite.studio>
2022-07-25 22:45:43 -0600

    Make prebuild hook project agnostic and detect whether the project has changed since last commit

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,7 +1,13 @@
 module.exports = {
   onPreBuild: ({ utils }) => {
-    const currentProject = 'client';
-    const projectHasChanged = false;
+    const currentProject = process.env.PROJECT_NAME;
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const latestCommit = 'HEAD';
+    const projectHasChanged = projectChanged(
+      currentProject,
+      lastDeployedCommit,
+      latestCommit
+    );
     if (!projectHasChanged) {
       utils.build.cancelBuild(
         `Build was cancelled because ${currentProject} was not affected by the latest changes`
@@ -9,3 +15,12 @@ module.exports = {
     }
   }
 };
+
+function projectChanged(currentProject, fromHash, toHash) {
+  const execSync = require('child_process').execSync;
+  const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+  const output = execSync(getAffected).toString();
+  //get the list of changed projects from the output
+  const changedProjects = JSON.parse(output).projects;
+  return !!changedProjects.find(project => project === currentProject);
+}
